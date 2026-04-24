98897e6ba6b3c106c54b78b854874b33b04ade27
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2026-04-24 19:11:08 +0100

    feat: add partner  logo

--- components/v2/partners.tsx
+++ components/v2/partners.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 const partners = [
   // { name: "DexPay", logo: "/assets/dexpay.svg" },
   { name: "BSC", logo: "/assets/BSC.svg" },
+  { name: "Stellar", logo: "/assets/stellar.svg" },
   { name: "Chase", logo: "/assets/chase.svg" },
   { name: "FCMB", logo: "/assets/fcmb.svg" },
   { name: "Base", logo: "/assets/base.svg" },
